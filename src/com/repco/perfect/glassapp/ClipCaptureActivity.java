3701d0f90726cfdf06308e4b4ec92a2213d52628
colin hom <colhom@gmail.com>
2014-03-30 01:01:04 -0700

    fucked...

--- src/com/repco/perfect/glassapp/ClipCaptureActivity.java
+++ src/com/repco/perfect/glassapp/ClipCaptureActivity.java
@@ -1,6 +1,5 @@
 package com.repco.perfect.glassapp;
 
-import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
@@ -8,47 +7,41 @@ import java.security.SecureRandom;
 import com.repco.perfect.glassapp.base.BaseBoundServiceActivity;
 
 import android.annotation.SuppressLint;
-import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
-import android.os.Environment;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
+
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class ClipCaptureActivity extends BaseBoundServiceActivity implements
-		MediaRecorder.OnInfoListener,
-		SurfaceHolder.Callback{
+		MediaRecorder.OnInfoListener, 
+		MediaRecorder.OnErrorListener,
+		SurfaceHolder.Callback,
+		GestureDetector.BaseListener {
 
-	private MediaRecorder mRec;
+	
 	private SurfaceView mPreviewSurface;
 
-	private File clipRoot = new File(Environment.getExternalStorageDirectory()
-			+ "/.perfect_cache");
 	
-	private String outputPath;
-	
-	@SuppressLint("TrulyRandom")
-	private final SecureRandom rand = new SecureRandom();
-	
+
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		destroyRecorder();
-		mClipService.abortClip(outputPath);
+		mClipService.destroyRecorder();
 	}
+	
+	@SuppressLint("TrulyRandom")
+	private final SecureRandom rand = new SecureRandom();
 
-	private void destroyRecorder(){
-		if(mRec != null){
-			mRec.stop();
-			mRec.release();
-			mRec = null;
-		}
-	}
+	String outputPath = ClipService.clipRoot.getAbsolutePath() + "/"
+			+ new BigInteger(130, rand).toString(32) + ".mp4";
 
 	@Override
 	public void onInfo(MediaRecorder mr, int what, int extra) {
 		switch (what) {
 		case MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED:
-			destroyRecorder();
 			mClipService.saveClip(outputPath);
 			finish();
 			break;
@@ -56,11 +49,10 @@ public class ClipCaptureActivity extends BaseBoundServiceActivity implements
 
 	}
 
-
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {}
-
+			int height) {
+	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
@@ -73,42 +65,47 @@ public class ClipCaptureActivity extends BaseBoundServiceActivity implements
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
-		}		
-		
+		}
 
 		mRec.start();
 	}
 
-
 	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {}
+	public void surfaceDestroyed(SurfaceHolder holder) {
+	}
 
-
+	private MediaRecorder mRec;
 	@Override
 	protected void onClipServiceConnected() {
 		setContentView(R.layout.capture);
 
-		if (!clipRoot.exists()) {
-			if (!clipRoot.mkdir()) {
-				throw new RuntimeException("Cannot mkdir "
-						+ clipRoot.getAbsolutePath());
-			}
-		}
-		outputPath = clipRoot.getAbsolutePath() + "/"
-				+ new BigInteger(130, rand).toString(32) + ".mp4";
 
-		mRec = new MediaRecorder();
-
-		mRec.setAudioSource(MediaRecorder.AudioSource.MIC);
-		mRec.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-		mRec.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
 		
-		mRec.setMaxDuration(4000);
-		mRec.setOnInfoListener(this);
 		mPreviewSurface = (SurfaceView) findViewById(R.id.capture_preview_surface);
 		mPreviewSurface.getHolder().addCallback(this);
-		mRec.setPreviewDisplay(mPreviewSurface.getHolder().getSurface());
+		
+		mRec = mClipService.getMediaRecorder();
+		mRec.setMaxDuration(4000);
 		mRec.setOutputFile(outputPath);
+		mRec.setPreviewDisplay(mPreviewSurface.getHolder().getSurface());
+		mRec.setOnInfoListener(this);
 	}
 
+	@Override
+	public boolean onGesture(Gesture arg0) {
+		switch (arg0) {
+		case SWIPE_DOWN:
+			mClipService.abortClip(outputPath);
+			break;
+		default:
+			break;
+		}
+		return false;
+	}
+
+	@Override
+	public void onError(MediaRecorder mr, int what, int extra) {
+		System.err.println("MediaRecorder onError: "+what+" "+extra);
+		
+	}
 }
--- src/com/repco/perfect/glassapp/ClipService.java
+++ src/com/repco/perfect/glassapp/ClipService.java
@@ -1,102 +1,181 @@
 package com.repco.perfect.glassapp;
 
 import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
+import com.google.android.glass.app.Card;
+import com.google.android.glass.media.Sounds;
 import com.google.android.glass.timeline.LiveCard;
 import com.google.android.glass.timeline.TimelineManager;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Camera;
+import android.media.AudioManager;
+import android.media.CamcorderProfile;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.widget.RemoteViews;
 
 public class ClipService extends Service {
 
-    private TimelineManager mTimelineManager;
-    private LiveCard mLiveCard;
+	private TimelineManager mTimelineManager;
+	private LiveCard mLiveCard;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mTimelineManager = TimelineManager.from(this);
-    }
-    private MediaPlayer openDinkSound; 
-    private MediaPlayer closeDinkSound;
-    public class ClipServiceBinder extends Binder{
-    
-    	public void stop(){
-    		ClipService.this.stopSelf();
-    	}
-    	
-    	public void recordClip(){	
-    		Intent captureIntent = new Intent(ClipService.this,ClipCaptureActivity.class);
-            captureIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            getApplication().startActivity(captureIntent);
-            openDinkSound.start();
-    	}
-    	
-    	public void abortClip(String outputPath){
-    		File f = new File(outputPath);
-    		f.delete();
-    		closeDinkSound.start();
-    	}
-    	
-    	public void saveClip(String outputPath){
-    		closeDinkSound.start();
-    	}
-    }
-    
-    private final ClipServiceBinder mBinder = new ClipServiceBinder();
-    
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mTimelineManager = TimelineManager.from(this);
+		for (int retry = 0; retry < 20; retry++) {
+			try {
+				System.out.println("Camera attempt #" + retry);
+				mCamera = Camera.open(); // attempt to get a Camera instance
+			} catch (Exception e) {
+				System.err.println(e);
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (mLiveCard == null) {
-            mLiveCard = mTimelineManager.createLiveCard("perfect");
+		if (mCamera == null) {
+			throw new RuntimeException("Couldn't get camera service");
+		}
+	}
 
-            mLiveCard.setDirectRenderingEnabled(false);
-            
-            mLiveCard.setViews(new RemoteViews(getPackageName(), R.layout.dash));
-            
-     
-            Intent menuIntent = new Intent(this, LaunchMenuActivity.class);
-            menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
+	private AudioManager am;
 
-            mLiveCard.publish(LiveCard.PublishMode.SILENT);
-        }
-        if (closeDinkSound == null){
-        	closeDinkSound = MediaPlayer.create(this, R.raw.fart);
-        }
-        if (openDinkSound == null){
-        	openDinkSound = MediaPlayer.create(this, R.raw.fart_open);
-        }
-        mBinder.recordClip();
+	public class ClipServiceBinder extends Binder {
 
-        return START_STICKY;
-    }
+		public void stop() {
+			ClipService.this.stopSelf();
+		}
 
-    @Override
-    public void onDestroy() {
-        if (mLiveCard != null && mLiveCard.isPublished()) {
+		public void recordClip() {
+			try {
+				mCamera.reconnect();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			try {
+				mCamera.setPreviewDisplay(null);
+			} catch (java.io.IOException ioe) {
+				System.err.println("IOException nullifying preview display: "
+						+ ioe.getMessage());
+			}
+			mCamera.stopPreview();
+			mCamera.unlock();
 
-            mLiveCard.unpublish();
-            mLiveCard = null;
-        }
-        if(openDinkSound != null){
-        	openDinkSound.release();
-        }
-        if(closeDinkSound != null){
-        	closeDinkSound.release();
-        }
-        super.onDestroy();
-    }
+			mRec = new MediaRecorder();
+			mRec.setCamera(mCamera);
+			mRec.setAudioSource(MediaRecorder.AudioSource.MIC);
+			mRec.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+			mRec.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+			Intent captureIntent = new Intent(ClipService.this,
+					ClipCaptureActivity.class);
+			captureIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+					| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			getApplication().startActivity(captureIntent);
+			am.playSoundEffect(Sounds.SELECTED);
+		}
+
+		public MediaRecorder getMediaRecorder() {
+			return mRec;
+		}
+
+		public void abortClip(String outputPath) {
+			try {
+				File f = new File(outputPath);
+				f.delete();
+			} finally {
+				destroyRecorder();
+			}
+		}
+
+		public void saveClip(String outputPath) {
+			am.playSoundEffect(Sounds.SUCCESS);
+			destroyRecorder();
+		}
+
+		public void destroyRecorder() {
+			if (mRec != null) {
+				try {
+					mRec.stop();
+					mRec.reset();
+					mRec.release();
+				} finally {					
+					mRec = null;
+					mCamera.unlock();
+				}
+			}
+		}
+	}
+
+	private final ClipServiceBinder mBinder = new ClipServiceBinder();
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
+
+	public static File clipRoot = new File(
+			Environment.getExternalStorageDirectory() + "/.perfect_cache");
+
+	private MediaRecorder mRec;
+	private Camera mCamera = null;
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+		if (mLiveCard == null) {
+			mLiveCard = mTimelineManager.createLiveCard("perfect");
+
+			mLiveCard.setDirectRenderingEnabled(false);
+
+			mLiveCard
+					.setViews(new RemoteViews(getPackageName(), R.layout.dash));
+
+			Intent menuIntent = new Intent(this, LaunchMenuActivity.class);
+			menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+					| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent,
+					0));
+
+			mLiveCard.publish(LiveCard.PublishMode.SILENT);
+		}
+		if (!clipRoot.exists()) {
+			if (!clipRoot.mkdir()) {
+				throw new RuntimeException("Cannot mkdir "
+						+ clipRoot.getAbsolutePath());
+			}
+		}
+
+		mBinder.recordClip();
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		if (mLiveCard != null && mLiveCard.isPublished()) {
+
+			mLiveCard.unpublish();
+			mLiveCard = null;
+		}
+		mBinder.destroyRecorder();
+		super.onDestroy();
+	}
 
 }
