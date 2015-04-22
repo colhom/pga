e045e993135854d5b231d8b15ff577b09929417d
Colin Hom <colhom@gmail.com>
2015-04-21 19:30:27 -0700

    production host, bumb version

--- perfectGlassApp/src/main/java/com/repco/perfect/glassapp/DevData.java
+++ perfectGlassApp/src/main/java/com/repco/perfect/glassapp/DevData.java
@@ -3,8 +3,8 @@ package com.repco.perfect.glassapp;
 public class DevData {
 	
 	//public static final String API_HOST = "http://ec2-54-215-189-167.us-west-1.compute.amazonaws.com:80";
-	public static final String API_HOST = "http://192.168.1.80:80";
-    //public static final String API_HOST = "https://www.chapterapp.io:443";
+	//public static final String API_HOST = "http://192.168.1.80:80";
+    public static final String API_HOST = "https://www.chapterapp.io:443";
     public static final String GOOGLE_ID = "102520547132235480567";
 	
 	 
